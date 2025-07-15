5a873c2b410865640a07bbd24e140956a05d3d75
Alfiya <al.yausheva@gmail.com>
2025-07-15 14:56:48 +0300

    +

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -15,16 +15,13 @@ text-decoration: ${({completed})=>(completed?"line-through":"none")}
 
 const OneTask: React.FC <TaskProps> = ({task, toggleTask, deleteTask}) => {
   return (
-
    <TaskItem completed={task.completed}>
-   
    <input type="checkbox" checked={task.completed} onChange={()=>toggleTask(task.id)}/>
    {task.text}
    <button onClick={()=>deleteTask(task.id)}>delete</button>
-
    </TaskItem>
 
   )
 }
 
-export default OneTask
+export default OneTask;
